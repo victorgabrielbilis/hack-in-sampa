c9edc7a4b03cfec5c3989dc689a3ce8a5b439596
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2018-06-17 02:20:57 -0300

    Design novo

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 	
 	function connect_DB()
 	{
-		$connection = mysqli_connect("localhost","root","bcd127","hackinsampa");
+		$connection = mysqli_connect("localhost","root","","hackinsampa");
 		mysqli_set_charset($connection,"utf8");
 		return $connection;
 	}
--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $botao="validar";
     <label>Casa</label>
     </div>
     <div class="input-field col s6">
-    <select name="itens">
+    <select name="itens" id="itens" onChange="changed();" class="materialSelect">
     <option value="0"> Selecione um item: </option>
     <?php 
         
@@ -68,7 +68,7 @@ $botao="validar";
         while($rs=mysqli_fetch_array($select))
         {
        
-        echo "<option value=".$rs['item'].">".$rs['item']."</option>"; 
+        echo "<option value=\"".$rs['item']."\">".$rs['item']."</option>"; 
 
         
         
@@ -80,12 +80,14 @@ $botao="validar";
   </div>
   <div class="row">
   <div class="input-field col s6">
-  <select>
-  <option value="" disabled selected></option>
-  <option value="1">Option 1</option>
-  <option value="2">Option 2</option>
-  <option value="3">Option 3</option>
-  </select>
+  <div id="select_fornecedor">
+    <select id="fornecedor">
+    <option value="" disabled selected></option>
+    <option value="1">Option 1</option>
+    <option value="2">Option 2</option>
+    <option value="3">Option 3</option>
+    </select>
+  </div>
   <label>Fornecedor</label>
   </div>
   <div class="input-field col s6">
@@ -99,6 +101,59 @@ $botao="validar";
     </div>
     </form>
   </div>
+  
+  <script>
+
+    function changed()
+    {
+      //alert();
+      var iten = document.getElementById('itens').value;
+      console.log("http://localhost/hack-in-sampa/api/v1/info/getFornecedor.php?despesa="+iten);
+      
+      $.get("http://localhost/hack-in-sampa/api/v1/info/getFornecedor.php?despesa="+iten, function(data, status){
+            //          alert("Data: " + data + "\nStatus: " + status);
+            console.log(data);
+            data = JSON.parse(data);
+
+            var html = "";
+            var i = 0;
+            
+            var elements = "";
+            while(i < data.lenght)
+            {
+                elemento = data[i].fornecedor;
+                elements += "<option value=\"\">"+elemento+"</option>";
+                i++;
+            }
+
+            
+      });
+      /*
+      $(document).ready(function() {
+            $('select').material_select();
+         });
+         */
+      //alert('chamei');  
+      /*
+       $('.materialSelect').material_select();
+
+      // setup listener for custom event to re-initialize on change
+      $('.materialSelect').on('contentChanged', function() {
+        $(this).material_select();
+      });   
+var $newOpt = $("<option>").attr("value",1).text("fdsafdsa")
+    $("#fornecedor").append($newOpt);
+  */
+/*
+        var x = document.getElementById("fornecedor");
+        var option = document.createElement("option");
+        option.text = "Kiwi";
+        x.add(option);
+        console.log('workou');
+        
+  */      
+    }
+  </script>
 
   <div class="row">
     <h5 class="col s12 black-text">Padrão:</h5>
@@ -122,7 +177,7 @@ $botao="validar";
                       text: 'Source: WorldClimate.com'
                     },
                     xAxis: {
-                      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                      categories: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
                     },
                     yAxis: {
                       title: {
@@ -139,10 +194,24 @@ $botao="validar";
                     },
                     series: [{
                       name: 'Tokyo',
-                      data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                    }, {
-                      name: 'London',
-                      data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                      data: [
+                      <?php
+
+                          $site = file_get_contents("http://localhost/hack-in-sampa/api/v1/despesas/getAllCostsMonths.php");
+                          $json = json_decode($site, true);
+                          $i = 0;
+                          $contents = "";
+                          while($i < count($json))
+                          {
+                            $contents .= $json[$i]['total'].",";
+                            $i++;
+                          }
+                          
+                          $contents = substr($contents, 0 , strlen($contents)-1);
+                          echo $contents."]";
+                      ?>
+
+                      //data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
                     }]
                   });
 
